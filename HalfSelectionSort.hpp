3e0d1b5db2a37d5e40211e935876c0ca219a8775
KarimE146 <karim666the@gmail.com>
2023-12-11 21:57:54 -0500

    test

--- HalfSelectionSort.hpp
+++ HalfSelectionSort.hpp
@@ -6,6 +6,32 @@
 #include <algorithm>
 #include <chrono>
 
-int halfSelectionSort ( std::vector<int>& nums, int& duration );
+int halfSelectionSort(std::vector<int>& nums, int& duration) {
+    auto start = std::chrono::high_resolution_clock::now();
 
+    if (nums.size() > 50000) {
+        std::cout << "The input was too big for selection sort.";
+    } else {
+        for (auto i = nums.begin(); i != nums.begin() + (nums.size() / 2) +1; ++i) {
+            auto min = i; // sets min to be i which iterated through the vector
+
+            for (auto j = i + 1; j != nums.end(); ++j) {
+                if (*j < *min) {
+                    min = j;
+                }
+            }
+            std::iter_swap(min, i);
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    duration = static_cast<int>(diff.count() * 1000); // Convert to milliseconds
+
+    if (nums.size() % 2 == 0) {
+        return *std::next(nums.begin(), (nums.size() / 2) - 1);
+    } else {
+        return *std::next(nums.begin(), nums.size() / 2);
+    } // Return the median
+}
 #endif
