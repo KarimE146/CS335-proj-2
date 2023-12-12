d5d9dca0794d3da3a2c15d79e44877a05a641412
KarimE146 <karim666the@gmail.com>
2023-12-11 21:52:50 -0500

    test

--- StandardSort.hpp
+++ StandardSort.hpp
@@ -6,6 +6,20 @@
 #include <algorithm>
 #include <chrono>
 
-int standardSort ( std::vector<int>& nums, int& duration );
+    int standardSort(std::vector<int>& nums, int& duration) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::sort(nums.begin(), nums.end());
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    duration = static_cast<int>(diff.count() * 1000);  // Convert to milliseconds
+
+    if (nums.size() % 2 == 0) {
+        return nums[(nums.size() / 2) - 1];
+    } else {
+        return nums[nums.size() / 2];
+    } // Return the median
+}
 
 #endif
