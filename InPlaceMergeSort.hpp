c48324e39ca35012acf90cd72e009d4951c79784
KarimE146 <karim666the@gmail.com>
2023-12-11 21:59:46 -0500

    test

--- InPlaceMergeSort.hpp
+++ InPlaceMergeSort.hpp
@@ -7,6 +7,31 @@
 #include <chrono>
 #include <iterator>
 
-int inPlaceMergeSort ( std::vector<int>& nums, int& duration );
+void inPlaceMergeSortRecursive(std::vector<int>& nums, std::vector<int>::iterator start, std::vector<int>::iterator end) {
+    if (start < end - 1) {
+        auto mid = start + std::distance(start, end) / 2;
+        inPlaceMergeSortRecursive(nums, start, mid); 
+        inPlaceMergeSortRecursive(nums, mid, end);   
+        std::inplace_merge(start, mid, end);    
+    }
+}
+
+
+int inPlaceMergeSort(std::vector<int>& nums, int& duration) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    inPlaceMergeSortRecursive(nums, nums.begin(), nums.end());
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    duration = static_cast<int>(diff.count() * 1000);
+
+     if (nums.size() % 2 == 0 )
+    {
+        return nums[(nums.size() / 2) -1];
+    }else{
+        return nums[nums.size() / 2];
+    } // Return the median
+}
 
 #endif
